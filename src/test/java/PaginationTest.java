5ab37318c6f183177a8fa7574f50d448643c7a34
DaxaA <stud.arhpva@gmail.com>
2021-11-10 18:53:05 +0300

    final version

--- src/test/java/PaginationTest.java
+++ src/test/java/PaginationTest.java
@@ -4,27 +4,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PaginationTest {
-    PersonList pagedPerson;
+    PersonList paged;
     PersonList personList = new PersonList();
 
     @BeforeEach
     void setUp() {
         personList.add(new Person("Nik", "Nos", Gen.male, 2002));
         personList.add(new Person("Artem", "Iv", Gen.male, 2001));
-        personList.add(new Person("Lena", "Shap", Gen.female, 2006));
+        personList.add(new Person("Lena", "Shape", Gen.female, 2006));
         personList.add(new Person("Dasha", "Iv", Gen.female, 2002));
-        personList.add(new Person("Alex", "Shap", Gen.male, 1999));
-        personList.add(new Person("Sergey", "Cheb", Gen.male, 2000));
+        personList.add(new Person("Alex", "Shape", Gen.male, 1999));
+        personList.add(new Person("Sergey", "Che", Gen.male, 2000));
         personList.add(new Person("Lena", "Kor", Gen.male, 1999));
-        personList.add(new Person("Nik", "Shap", Gen.male, 2000));
+        personList.add(new Person("Nik", "Shape", Gen.male, 2000));
     }
 
     @Test
     void truePage() {
-        pagedPerson = Pagination.paginationList(3, 2, personList);
-        assertEquals(3, pagedPerson.getPersonList().size());
-        assertEquals("Dasha", pagedPerson.getPersonList().get(0).getFirstName());
-        assertEquals("Alex", pagedPerson.getPersonList().get(1).getFirstName());
-        assertEquals("Sergey", pagedPerson.getPersonList().get(2).getFirstName());
+        paged = Pagination.paginationList(3, 2, personList);
+        assertEquals(3, paged.getPersonList().size());
+        assertEquals("Dasha", paged.getPersonList().get(0).getFirstName());
+        assertEquals("Alex", paged.getPersonList().get(1).getFirstName());
+        assertEquals("Sergey", paged.getPersonList().get(2).getFirstName());
     }
 }
